17361bf72504bd9ddd236b853f64943df7f7a84d
muxin.lc <muxin.lc@koubei.com>
2019-09-07 16:04:50 +0800

    优化

---
+++ src/main/resources/static/precache-manifest.465ac5837bbf8b849cee372351cab224.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff8ee2bfb7afa6bfbcc2",
+    "url": "/p__recipe__EnumInfo.chunk.css"
+  },
+  {
+    "revision": "046fe9a45415caa244dd",
+    "url": "/vendors.chunk.css"
+  },
+  {
+    "revision": "ff8ee2bfb7afa6bfbcc2",
+    "url": "/p__recipe__EnumInfo.async.js"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "/pwacompat.min.js"
+  },
+  {
+    "revision": "71d99393ec8a3688a207",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "71d99393ec8a3688a207",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "046fe9a45415caa244dd",
+    "url": "/vendors.async.js"
+  },
+  {
+    "revision": "f90e39620750235269a0",
+    "url": "/p__recipe__RecipeManage.chunk.css"
+  },
+  {
+    "revision": "580d5c94274e3a582ef0",
+    "url": "/p__Exception__404.async.js"
+  },
+  {
+    "revision": "40115e141ccec8c470b2",
+    "url": "/p__Exception__TriggerException.chunk.css"
+  },
+  {
+    "revision": "40115e141ccec8c470b2",
+    "url": "/p__Exception__TriggerException.async.js"
+  },
+  {
+    "revision": "b5a19506937c5094262c",
+    "url": "/p__Exception__models__error.js.async.js"
+  },
+  {
+    "revision": "18a57ce91d6cfff340e5",
+    "url": "/p__User__Login.chunk.css"
+  },
+  {
+    "revision": "18a57ce91d6cfff340e5",
+    "url": "/p__User__Login.async.js"
+  },
+  {
+    "revision": "cfd4c8b0a6187bf314a6",
+    "url": "/p__User__Register.chunk.css"
+  },
+  {
+    "revision": "cfd4c8b0a6187bf314a6",
+    "url": "/p__User__Register.async.js"
+  },
+  {
+    "revision": "e0bad70e9814ab446673",
+    "url": "/p__User__RegisterResult.chunk.css"
+  },
+  {
+    "revision": "e0bad70e9814ab446673",
+    "url": "/p__User__RegisterResult.async.js"
+  },
+  {
+    "revision": "a342a71135a3e038ff43",
+    "url": "/p__User__models__register.js.async.js"
+  },
+  {
+    "revision": "80b37b0071cba854dc06",
+    "url": "/p__recipe__ChineseMedicine.chunk.css"
+  },
+  {
+    "revision": "80b37b0071cba854dc06",
+    "url": "/p__recipe__ChineseMedicine.async.js"
+  },
+  {
+    "revision": "a2f38dd9ba43fa068369",
+    "url": "/p__recipe__WesternMedicine.async.js"
+  },
+  {
+    "revision": "a2f38dd9ba43fa068369",
+    "url": "/p__recipe__WesternMedicine.chunk.css"
+  },
+  {
+    "revision": "7b8659f28c445193eeb0",
+    "url": "/p__recipe__RecipeEdit.chunk.css"
+  },
+  {
+    "revision": "7b8659f28c445193eeb0",
+    "url": "/p__recipe__RecipeEdit.async.js"
+  },
+  {
+    "revision": "32bff45f6e450442e134",
+    "url": "/p__Exception__500.async.js"
+  },
+  {
+    "revision": "f90e39620750235269a0",
+    "url": "/p__recipe__RecipeManage.async.js"
+  },
+  {
+    "revision": "c6b030639f056de04c7b",
+    "url": "/p__recipe__Template.async.js"
+  },
+  {
+    "revision": "7cc6441069ac8047103b",
+    "url": "/p__recipe__TemplateAdd.async.js"
+  },
+  {
+    "revision": "3279bbf62ffa5d4abe57",
+    "url": "/p__recipe__TemplateUpdate.async.js"
+  },
+  {
+    "revision": "ee97857a1c4c0203bba4fc53018b7c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c373561e413f97400076",
+    "url": "/layouts__BasicLayout.chunk.css"
+  },
+  {
+    "revision": "1dfae319f37b4acef310",
+    "url": "/layouts__UserLayout.async.js"
+  },
+  {
+    "revision": "1dfae319f37b4acef310",
+    "url": "/layouts__UserLayout.chunk.css"
+  },
+  {
+    "revision": "0e530af6fe2f20b65c32fcee3b608160",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "a891c0a6ff56440598af",
+    "url": "/p__404.async.js"
+  },
+  {
+    "revision": "c373561e413f97400076",
+    "url": "/layouts__BasicLayout.async.js"
+  },
+  {
+    "revision": "5661ce8e2472d2727e8fd0390b22ba37",
+    "url": "/icons/icon-512x512.png"
+  },
+  {
+    "revision": "c23aa881b8d0e959f6c0aeb5ae7e4baa",
+    "url": "/icons/icon-192x192.png"
+  },
+  {
+    "revision": "347329d0982682b42d40c8162817a940",
+    "url": "/icons/icon-128x128.png"
+  },
+  {
+    "revision": "694c4300be571fdd4d8b6abbe14d2e3f",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "4c43d4387599007f6da1",
+    "url": "/21.chunk.css"
+  },
+  {
+    "revision": "4c43d4387599007f6da1",
+    "url": "/21.async.js"
+  }
+];
